7ecd246438c3fcff4cbdef5147aba870980c69b4
Xanneth <Xanneth@DUCHESS>
2016-04-09 09:47:04 -0400

    Create the database selection and validation dialogue.

--- GameClient/MainWindow.xaml.cs
+++ GameClient/MainWindow.xaml.cs
@@ -25,11 +25,29 @@ namespace GameClient
             InitializeComponent();
         }
 
+       
         private async void Button_FindDatabase_Click(object sender, RoutedEventArgs e)
         {
+            //First ask the client to find a database for us.
             Microsoft.Win32.OpenFileDialog dialog = new Microsoft.Win32.OpenFileDialog();
             dialog.ShowDialog();
-            MessageBox.Show((await GameBackend.Diagnostics.TestDatabaseConnectionAsync(dialog.FileName)).ToString());
+
+            //We're going to pass the result into the view model now.
+            (this.DataContext as MainWindowViewModel).DatabaseLocation = dialog.FileName;
+
+            //And finally, we're going to ask the backend if the database is valid and change the text block status indicator accordingly
+            if (await GameBackend.Diagnostics.TestDatabaseConnectionAsync((this.DataContext as MainWindowViewModel).DatabaseLocation))
+            {
+                //The connection was valid. Woohoo
+                TextBlock_DatabaseStatus.Text = "Database Valid";
+                TextBlock_DatabaseStatus.Background = new SolidColorBrush(Colors.Green);
+            }
+            else
+            {
+                // :(  so sad, no connection
+                TextBlock_DatabaseStatus.Text = "Database Invalid";
+                TextBlock_DatabaseStatus.Background = new SolidColorBrush(Colors.Red);
+            }
         }
 
         private void Button_Settings_Click(object sender, RoutedEventArgs e)
